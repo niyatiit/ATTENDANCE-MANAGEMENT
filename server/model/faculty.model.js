aa379a63c040cccfac93bd18901aef9d015056c8
Niyati_Patel <130044323+niyatiit@users.noreply.github.com>
2025-02-16 01:51:28 +0530

    Update faculty.model.js

--- server/model/faculty.model.js
+++ server/model/faculty.model.js
@@ -25,10 +25,6 @@ const facultySchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
   },
   {
     timestamps: true,
