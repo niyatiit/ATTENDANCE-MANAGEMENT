cd9fe8d5c442411bc948cab71132e064fbc5faec
Niyati_Patel <130044323+niyatiit@users.noreply.github.com>
2025-02-16 00:07:37 +0530

    Update db.js connection url added

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 const connectDB=async()=>{
     try{
-        const conn=await mongoose.connect('mongodb://localhost:27017/hackathon-project');
+        const conn=await mongoose.connect('mongodb+srv://niyatipatel0701:L7pOmBifCE1C3X6e@cluster0.hepr5.mongodb.net');
         console.log(`Database Connected`);
     }
     catch(err){
