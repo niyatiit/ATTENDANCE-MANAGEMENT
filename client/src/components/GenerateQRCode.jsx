93d8384b6912bb78715fd9e0ed7d442ff463202e
Niyati_Patel <130044323+niyatiit@users.noreply.github.com>
2025-02-16 02:33:55 +0530

    Update GenerateQRCode.jsx

--- client/src/components/GenerateQRCode.jsx
+++ client/src/components/GenerateQRCode.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import QRCode from "react-qr-code";
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
 
 function GenerateQRCode() {
 
   // Todo: Faculty authentication
 const user = JSON.parse(localStorage.getItem("user") || "{}");
+  const [subject, setSubject] = useState([]);
 
   const value = {
     "facultyId": user?._id || "Nahi hai",
@@ -29,14 +31,30 @@ const user = JSON.parse(localStorage.getItem("user") || "{}");
     return () => clearInterval(interval);
   }, []);
 
+  useEffect (() => {
+    axios.post("https://attendance-management-nine.vercel.app/getSubject", {facultyId: user?._id}, {withCredentials: true})
+    .then((res) => {
+      console.log(res);
+    })
+    .catch ((error) => {
+      console.log(error);
+    });
+  }, []);
+
   return (
     <div style={{ background: 'white', padding: '16px' }}>
-      <QRCode 
-        size={256}
-        style={{ height: "500px", maxWidth: "100%", width: "100%" }}
-        value={qrValue}
-        viewBox={`0 0 256 256`}
-      />
+      {
+        
+      }
+      {
+        subject.length > 0 && 
+        <QRCode 
+          size={256}
+          style={{ height: "500px", maxWidth: "100%", width: "100%" }}
+          value={qrValue}
+          viewBox={`0 0 256 256`}
+        />
+      }
     </div>
   );
 }
