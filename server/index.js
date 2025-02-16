38db77d3f4bd190292d19571a3cbc1d062bffdd7
Niyati_Patel <130044323+niyatiit@users.noreply.github.com>
2025-02-16 09:49:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://magenta-souffle-b73880.netlify.app', 
   credentials: true,
 }));
 const port = 3000;
